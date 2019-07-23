b277b32223ee5ced4cb274847b016ba4b6e028c7
Mark <mazeal@hotmail.com>
2019-07-22 22:44:16 -0400

    Date formatting update and handle sensor reading failures.

--- sensorPython.py
+++ sensorPython.py
@@ -16,7 +16,7 @@ from email.utils import COMMASPACE, formatdate
 from email import encoders
 
 #imports for temperature
-#import Adafruit_DHT
+import Adafruit_DHT
 
 #Globals
 with open("email_password.txt", 'r') as file:
@@ -24,10 +24,9 @@ with open("email_password.txt", 'r') as file:
 #update to accept more than a single email from file
 with open("destination_email_list.txt", 'r') as file:
   mail_list = [line.split(', ') for line in file.readlines()]
-
 email_time_sec = 68400 # 7 pm in seconds
 record_freq = 60
-#sensor_1 = sensor_2 = Adafruit_DHT.DHT22
+sensor_1 = sensor_2 = Adafruit_DHT.DHT22
 pin_1 = 17
 pin_2 = 23
 DATAFILE = "temperature_data.txt"
@@ -40,22 +39,37 @@ SEVEN_DAYS_IN_SEC = 604800
 
 def record_temperature(file_name):
   try:
-    humidity_1, temperature_1 = (1, 2) #Adafruit_DHT.read_retry(sensor_1, pin_1) #round to nearest int
-    humidity_2, temperature_2 = (3, 4) #Adafruit_DHT.read_retry(sensor_2, pin_2) #round to nearest int
-    file_name.write("%d/%d - %d:%d:%d Ambient Temp %d Ambient Humidity %d Fridge Temp %d\r\n" % \
-      (time.localtime(time.time()).tm_mon, time.localtime(time.time()).tm_mday, \
-        time.localtime(time.time()).tm_hour, time.localtime(time.time()).tm_min, \
-          time.localtime(time.time()).tm_sec, temperature_1, humidity_1, temperature_2))
+    humidity_1, temperature_1 = Adafruit_DHT.read_retry(sensor_1, pin_1) #round to nearest int
+    humidity_2, temperature_2 = Adafruit_DHT.read_retry(sensor_2, pin_2) #round to nearest int
+    if humidity_1 is None or temperature_1 is None:
+        humidity_1 = 100
+        temperature_1 = 100
+    if humidity_2 is None or temperature_2 is None:
+        humidity_2 = 100
+        temperature_2 = 100   
+    print(temperature_1, "   ",temperature_2)
   except RuntimeError as e:
     file_name.write("Error reading sensor!\r\n")
-  
+  else:
+    if temperature_1 == None:
+        temperature_1 = 100
+    temperature_1=temperature_1 * 9/5.0 + 32
+    temperature_2=temperature_2 * 9/5.0 + 32
+    file_name.write("%s Ambient Temp %d Ambient Humidity %d Fridge Temp %d\r\n" % \
+      (time.strftime("%b %d - %H:%M:%S"), temperature_1, humidity_1, temperature_2))
+
 ##########################################
 #FUNCTION TO ADD UPTIME TO FILE
 ##########################################
 
 def add_uptime_to_file(file_name):
   uptime = time.time() - BOOT_TIME
-  file_name.write("Total Uptime (sec) = %d" % uptime)
+  days = int(uptime / 86400)
+  hours = int(uptime % 86400 / 3600)
+  minutes = int(uptime % 3600 / 60)
+  seconds = int(uptime % 60)
+  with open(file_name, "a+") as file:
+      file.write("Total Uptime (D:H:M:S) = %d:%d:%02d:%02d" % (days, hours, minutes, seconds))
 
 ##########################################
 #FUNCTION TO SEND EMAILS
@@ -73,7 +87,9 @@ def send_email(mail_list):
     msg['From'] = SRC_USERNAME
     msg['To'] = email_address
     msg.attach ( MIMEText(text) )
-
+    print("email_address")
+    print(email_address)
+    print("--------")
     for file in files:
       part = MIMEBase('application', "octet-stream")
       part.set_payload(open(file,"rb").read())
@@ -81,7 +97,6 @@ def send_email(mail_list):
       part.add_header('Content-Disposition', 'attachment; filename="%s"'\
         % os.path.basename(file))
       msg.attach(part)
-
     server = smtplib.SMTP('smtp.gmail.com:587')
     server.ehlo_or_helo_if_needed()
     server.starttls()
@@ -118,14 +133,13 @@ while True:
     measurement_taken = False
 
   # add the uptime to the file before emailing
-  if time.localtime(time.time()).tm_hour == 11 and email_sent_today == False:
-    file_name = open(DATAFILE, "a+")
-    add_uptime_to_file(file_name)
+  if time.localtime(time.time()).tm_hour == 15 and email_sent_today == False:
+    add_uptime_to_file(DATAFILE)
     send_email(mail_list[0])
     os.remove(DATAFILE)
     email_sent_today = True
-  elif time.localtime(time.time()).tm_hour == 12 and email_sent_today == True:
+  elif time.localtime(time.time()).tm_hour == 16 and email_sent_today == True:
     email_sent_today = False
 
-  if time.localtime(time.time()).tm_wday == 5 and (time.time()-BOOT_TIME) > SEVEN_DAYS_IN_SEC:
+  if time.localtime(time.time()).tm_wday == 1 and (time.time()-BOOT_TIME) > SEVEN_DAYS_IN_SEC:
     restart_pi()
